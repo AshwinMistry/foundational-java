4853eff26138933e8bea0c286c236b3aeeeae740
Ashwin Mistry <ashwin.mistry@macmillan.com>
2016-02-26 14:08:19 +0000

    initial commit

---
+++ workspace/MyJavaProject/src/com/introjava/chapter2/MyJavaProgram.java
@@ -0,0 +1,10 @@
+package com.introjava.chapter2;
+
+public class MyJavaProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("My Java program running!");
+	}
+
+}
